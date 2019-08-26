a49227b4c3acc029d6515e7fd62da952db6b68b6
maguohao2018 <guohao.ma@fit2cloud.com>
2019-08-26 17:07:19 +0800

    shiro 设置

--- src/main/java/com/fit2cloud/config/controller/UserController.java
+++ src/main/java/com/fit2cloud/config/controller/UserController.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
-@RequestMapping("impersonateLogin")
+@RequestMapping("anonymous/impersonateLogin")
 public class UserController {
 
     @Resource
